f94b967e8c2612a851429b4f8848ae636ff6aa11
Shak <shakal802@yahoo.com>
2017-02-22 19:50:43 -0500

    In-Class Assignment 2/22 make toActivity

--- app/src/main/java/com/example/shak/shakaldemo/MainActivity.java
+++ app/src/main/java/com/example/shak/shakaldemo/MainActivity.java
@@ -14,6 +14,10 @@ import butterknife.OnClick;
 public class MainActivity extends BaseActivity {
     private ImageButton bt1;
     private ImageButton bt3;
+    private ImageButton bt4;
+
+
+
 
     @OnClick(R.id.bt2)
     public void button2Click(){
@@ -34,6 +38,8 @@ public class MainActivity extends BaseActivity {
     private void initialView() {
         bt1 = (ImageButton) findViewById(R.id.bt1);
         bt3 = (ImageButton) findViewById(R.id.bt3);
+        bt4 = (ImageButton) findViewById(R.id.bt4);
+
     }
 
     @Override
@@ -70,6 +76,16 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                Intent intent = new Intent(v.getContext(),toActivity.class);
+                startActivityForResult(intent,4);
+                //  Intent intent = new Intent(v.getContext(),ListViewActivity.class);
+                //  startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -86,6 +102,8 @@ public class MainActivity extends BaseActivity {
             case 3:
                 toastShort("ListView");
                 break;
+            case 4:
+                toastShort("To Activity");
             default:
         }
     }
